0996912470018afd85781b5002776841488d699f
MD <maximiliandillmann144@gmail.com>
2026-07-21 10:13:24 +0200

    added release functionality

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using CaseInvestmentTracker.ViewModels;
+
+namespace CaseInvestmentTracker
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // ViewModel mit der UI verbinden
+            DataContext = new MainViewModel();
+        }
+    }
+}
---
+++ app.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace CaseInvestmentTracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Hauptfenster erstellen
+            MainWindow mainWindow = new MainWindow();
+
+            // Fenster anzeigen
+            mainWindow.Show();
+        }
+    }
+}
